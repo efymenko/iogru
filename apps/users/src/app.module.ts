37fab0c44419b7e380b911757905983551da0e4c
Egor Efimenko <efymenko@vk.com>
2022-06-08 18:32:31 +0600

    refactor(users): add minor changes

--- apps/users/src/app.module.ts
+++ apps/users/src/app.module.ts
@@ -1,9 +1,14 @@
-import { LoggerModule } from '@iogru/logger';
-import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
+// local
 import { Env, validate } from './app.env-validator';
 import { UsersModule } from './users/users.module';
+
+// libs
+import { LoggerModule, LoggerService } from '@iogru/logger';
+
+// global
+import { Module, OnApplicationShutdown } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
@@ -13,6 +18,8 @@ import { UsersModule } from './users/users.module';
       inject: [ConfigService],
       useFactory: async (configService: ConfigService<Env, true>) => ({
         uri: configService.get('MONGO_URI', { infer: true }),
+        retryAttempts: 1,
+        retryDelay: 1000,
       }),
     }),
     ConfigModule.forRoot({
@@ -24,4 +31,13 @@ import { UsersModule } from './users/users.module';
     }),
   ],
 })
-export class AppModule {}
+export class AppModule implements OnApplicationShutdown {
+  constructor(private readonly logger: LoggerService) {}
+
+  /**
+   * @required `app.enableShutdownHooks()` into [main.ts](./main.ts)
+   */
+  onApplicationShutdown(signal?: string) {
+    this.logger.crit(`The apllication shut down with ${signal} signal.`);
+  }
+}
