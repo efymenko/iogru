2b3c8766f88716eafac0b18522933957c37aa1d2
Egor Efimenko <efymenko@vk.com>
2022-06-15 16:39:37 +0600

    fix(common): update re-exports

--- libs/common/src/index.ts
+++ libs/common/src/index.ts
@@ -2,3 +2,5 @@ export * from './props';
 export * from './params';
 export * from './pipes';
 export * from './filters';
+export * from './exceptions-transform-factory';
+export * from './exceptions';
